736a26c9de0b5d1bf67377bb7f7ff98fdc68e1b6
Robert Gomez <rtirado@altice.com.do>
2023-08-03 15:23:02 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demo-ion-radio-label-center',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
